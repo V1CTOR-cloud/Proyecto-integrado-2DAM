8ac5496c3b644dd3be4f5dd5359c323bdf40d4b6
V1CTOR-cloud <latipicacuenta89@gmail.com>
2022-01-14 18:01:16 +0100

    Creando pantalla de carga

--- App/DailySense/App.js
+++ App/DailySense/App.js
@@ -6,106 +6,88 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+ import React, {useRef, useEffect} from 'react';
+ import {
+   SafeAreaView,
+   Image,
+   Text,
+   View,
+   StyleSheet,
+   Dimensions,
+   Animated,
+ } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App: () => React$Node = () => {
+  const moveAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: Dimensions.get('window').width / 1.6,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+      Animated.timing(moveAnim, {
+        duration: 2000,
+        toValue: 0,
+        delay: 0,
+        useNativeDriver: false,
+      }),
+    ]).start();
+    Animated.timing(fadeAnim, {
+      duration: 2000,
+      toValue: 1,
+      delay: 2000,
+      useNativeDriver: false,
+    }).start();
+  }, [moveAnim, fadeAnim]);
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Animated.Image
+          style={[styles.image, {opacity: fadeAnim}]}
+          source={require('./img/logo_simplificado_PNG.png')}
+        />
+        <Animated.View style={[styles.logoContainer, {marginLeft: moveAnim}]}>
+          <Text style={[styles.logoText]}>D</Text>
+          <Animated.Text style={[styles.logoText, {opacity: fadeAnim}]}>
+            ailySense
+          </Animated.Text>
+        </Animated.View>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  logoText: {
+    fontSize: 35,
+    marginTop: -20,
+    color: '#444444',
+    fontWeight: '300',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  contentContainer: {
+    top: '25%',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  image: {
+    width: 275,
+    height: 275,
+  },
+  logoContainer: {
+    flexDirection: 'row',
   },
 });
 
