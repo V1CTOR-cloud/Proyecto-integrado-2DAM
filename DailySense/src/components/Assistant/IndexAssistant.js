3cd7e446e01338d6f40f7c2e6e1e7e2f78a1ba5d
ALUMNADO\rupeca01 <rupeca01@floridauniversitaria.es>
2022-02-04 16:10:20 +0100

    A

--- DailySense/src/components/Assistant/IndexAssistant.js
+++ DailySense/src/components/Assistant/IndexAssistant.js
@@ -11,23 +11,38 @@ const IndexAssistant = () => {
 
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.logo}
-                source={require("../../assets/img/Dependiente.png")} />
+            <View style={styles.header}>
+                <Text style={styles.titol}>Bienvenido JUAN</Text>
+                <Image
+                    style={styles.logo}
+                    source={require("../../assets/img/Dependiente.png")} />
+            </View>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
+    },
+    header:{
+        flex:0.3,
+        flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    titol:{
+        left:40,
+        fontSize:25,
+        color:"black",
     },
     logo: {
-        position:"absolute",
         top: 0,
+        left:20,
         right: 0,
-        width: "40%",
-        height: "30%",
+        width: "50%",
+        height: "80%",
         resizeMode: "stretch",
     },
 })
