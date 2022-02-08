b1ec600ba049b96163282667e8e4aecaacde9120
ALUMNADO\neluva <neluva@floridauniversitaria.es>
2022-02-08 18:22:16 +0100

    no message

--- BBDD/bdPI.sql
+++ BBDD/bdPI.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS DailySense;
 
 USE DailySense;
 
-CREATE TABLE Grandpa(
-IdGrandpa INT() NOT NULL auto_increment,
+CREATE TABLE Dependents(
+IdDependents INT(4) NOT NULL auto_increment,
 Name VARCHAR(45) NOT NULL,
 LastName VARCHAR(90)NOT NULL,
 Address VARCHAR(45)NOT NULL,
@@ -12,36 +12,30 @@ Age INT(3) NOT NULL,
 FamilyContact INT(20),
 Diseases VARCHAR(200),
 Allergies VARCHAR(200),
+Assistant INT (4) NOT NULL,
 DependencyLevel VARCHAR(200),                    
 PRIMARY KEY(IdGrandpa),
-FOREIGN KEY(IdGrandpa) references Atributes(Grandpa)   
+FOREIGN KEY(IdGrandpa) references Attributes(Grandpa)   
 );
 
-CREATE TABLE Relation(
-IdAssitant INT() NOT NULL ,
-IdGrandpa INT() NOT NULL,
-PRIMARY KEY(IdAssitant,IdGrandpa),
-FOREIGN KEY(IdAssitant) references Assistant(IdAssitant),   
-FOREIGN KEY(IdGrandpa) references Grandpa(IdGrandpa)        
-);
 
 CREATE TABLE Assistant(
-IdAssitant INT() NOT NULL auto_increment,
+IdAssitant INT(4) NOT NULL auto_increment,
 User VARCHAR(30) NOT NULL,
 Password VARCHAR(30) NOT NULL,
-Dependents INT(3),
 Gender VARCHAR(10),
 Email VARCHAR(45),
-PRIMARY KEY(IdAssitant)
+PRIMARY KEY(IdAssitant),
+FOREIGN KEY(IdAssistant) references Dependents(Assistant)
 );
 
-CREATE TABLE Atributes(
-IdAtribute INT() NOT NULL auto_increment,
-Type INT NOT NULL,
+CREATE TABLE Attributes(
+IdAtribute INT(4) NOT NULL auto_increment,
+Type INT(1) NOT NULL,
 Name VARCHAR(30) NOT NULL,
 Description VARCHAR(400),
-Grandpa VARCHAR(10),           
-Date DATETIME(),
+Grandpa INT (4)NOT NULL,           
+Date DATETIME ,
 PRIMARY KEY(IdAtribute)
 );
 
