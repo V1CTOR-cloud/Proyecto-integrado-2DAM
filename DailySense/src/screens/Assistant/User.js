1065096dec926c741891b93f40c33848fdca7170
DESKTOP-7A6IGAV\ASUS <victorbg189@gmail.com>
2022-02-11 20:25:30 +0100

    Añadiendo pantalla USER

--- DailySense/src/screens/Assistant/User.js
+++ DailySense/src/screens/Assistant/User.js
@@ -31,28 +31,74 @@ const User = ({ navigation }) => {
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.themeColor} />
       <View style={styles.header}>
-        <Image
-          source={require('../../assets/img/wall.jpg')}
-          style={styles.wall}
-        />
-        <Text style={styles.h1}>Victor</Text>
+        <View style={styles.contheader}>
+          <Image
+            source={require('../../assets/img/user2.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.h1}>Victor</Text>
+        </View>
       </View>
       <View style={styles.body}>
-        <View style={styles.texti}>
-          <Image
-            style={styles.img}
-            source={require('../../assets/img/lock.png')}
-          />
-          <TextInput
-            placeholder='Your Password goes here...'
-            style={styles.box}
-            label='Password'
-            mode='outlined'
-            value={Password}
-            onChangeText={Password => setPassword(Password)}
-            secureTextEntry={true}
-            theme={{ colors: { primary: colors.tint } }}
-          />
+        <View style={styles.cont}>
+          <View style={styles.texti}>
+            <Image
+              style={styles.img}
+              source={require('../../assets/img/mail.png')}
+            />
+            <TextInput
+              placeholder='Your Email goes here...'
+              style={styles.box}
+              label='Email'
+              mode='outlined'
+              theme={{ colors: { primary: colors.tint } }}
+            />
+          </View>
+          <View style={styles.texti}>
+            <Image
+              style={styles.img}
+              source={require('../../assets/img/user.png')}
+            />
+            <TextInput
+              placeholder='Your Username goes here...'
+              style={styles.box}
+              label='Username'
+              mode='outlined'
+              value={User}
+              onChangeText={User => setUser(User)}
+              theme={{ colors: { primary: colors.tint } }}
+            />
+          </View>
+          <View style={styles.texti}>
+            <Image
+              style={styles.img}
+              source={require('../../assets/img/lock.png')}
+            />
+            <TextInput
+              placeholder='Your Password goes here...'
+              style={styles.box}
+              label='Password'
+              mode='outlined'
+              value={Password}
+              onChangeText={Password => setPassword(Password)}
+              secureTextEntry={true}
+              theme={{ colors: { primary: colors.tint } }}
+            />
+          </View>
+          <View style={styles.texti}>
+            <Image
+              style={styles.img}
+              source={require('../../assets/img/lock.png')}
+            />
+            <TextInput
+              placeholder='Confirm your password'
+              style={styles.box}
+              label='Confirm password'
+              mode='outlined'
+              secureTextEntry={true}
+              theme={{ colors: { primary: colors.tint } }}
+            />
+          </View>
         </View>
       </View>
       <View style={styles.footer}>
@@ -67,51 +113,63 @@ const User = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.background,
+    backgroundColor: colors.themeColor,
     justifyContent: 'center',
     alignItems: 'center'
   },
   header: {
-    flex: 1,
+    flex: 0.8,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    borderBottomLeftRadius: 80,
-    borderBottomRightRadius: 80,
     backgroundColor: colors.themeColor,
+  },
+  contheader: {
+    height: 50,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 10,
+    flexDirection: 'row',
+    padding: 5
   },
   h1: {
     fontSize: 30,
     fontWeight: 'bold',
     color: colors.white,
-    position: 'absolute',
   },
   body: {
-    flex: 4,
-    width: '100%',
+    flex: 3,
+    width: '94%',
     backgroundColor: colors.background,
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    justifyContent: 'center',
+    padding: 20,
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+    borderBottomLeftRadius: 60,
+    borderBottomRightRadius: 60
+  },
+  cont: {
+
   },
   footer: {
     flex: 1,
     width: '100%',
-    backgroundColor: colors.tint,
+    
   },
   wall: {
-    height: 130,
-    width: '100%',
-    borderBottomLeftRadius: 80,
-    borderBottomRightRadius: 80,
+    height: 112,
+    width: '101%',
     position: 'absolute',
     top: 0,
-    opacity: 0.5
+    opacity: 0.3
   },
   texti: {
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    padding: 10
   },
   box: {
     height: 45,
@@ -121,6 +179,12 @@ const styles = StyleSheet.create({
   img: {
     height: 20,
     width: 20,
+  },
+  logo: {
+    height: 40,
+    width: 40,
+    position: 'relative',
+    right: 20
   },
 });
 
