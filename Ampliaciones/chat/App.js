47e42cea315b0513870171d19d92ddbe127d6bbb
DESKTOP-7A6IGAV\ASUS <victorbg189@gmail.com>
2022-04-10 22:27:28 +0200

    Problema solucionado ( NativeBase no funcionaba )

---
+++ Ampliaciones/chat/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, Box } from "native-base";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return ( 
+        <NativeBaseProvider>
+          <Box style={styles.container}>Hello world</Box>
+        </NativeBaseProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
