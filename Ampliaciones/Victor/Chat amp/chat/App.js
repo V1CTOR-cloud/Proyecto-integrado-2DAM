d98dfc2f837d4f2b9e31293bd41381b46fad13f4
DESKTOP-7A6IGAV\ASUS <victorbg189@gmail.com>
2022-04-18 12:50:22 +0200

    Borrado de proyecto entero ( CHAT ) y regeneración del mismo

--- Ampliaciones/Victor/Chat amp/chat/App.js
+++ Ampliaciones/Victor/Chat amp/chat/App.js
@@ -1,20 +1,20 @@
-import Main from "./src/Main";
-import Chat from "./src/Chat";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Chat" component={Chat} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
