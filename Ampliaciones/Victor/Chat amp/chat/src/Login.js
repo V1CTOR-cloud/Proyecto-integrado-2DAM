8d36213c587e7194491f5823354b1dd783675194
DESKTOP-7A6IGAV\ASUS <victorbg189@gmail.com>
2022-04-19 19:22:52 +0200

    Subiendo cambios

---
+++ Ampliaciones/Victor/Chat amp/chat/src/Login.js
@@ -0,0 +1,15 @@
+import { NativeBaseProvider, Box } from "native-base";
+
+
+function Login() {
+    return ( 
+        <NativeBaseProvider>
+            <Box>Hello world</Box>
+        </NativeBaseProvider>
+     );
+}
+
+
+
+
+export default Login;
