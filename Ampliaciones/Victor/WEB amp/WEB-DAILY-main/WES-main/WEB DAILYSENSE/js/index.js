cccfdfe1e32b10518d59d35d3f691356ea94d645
DESKTOP-7A6IGAV\ASUS <victorbg189@gmail.com>
2022-05-25 20:03:01 +0200

    Subiendo cambios a licencia html

--- Ampliaciones/Victor/WEB amp/WEB-DAILY-main/WES-main/WEB DAILYSENSE/js/index.js
+++ Ampliaciones/Victor/WEB amp/WEB-DAILY-main/WES-main/WEB DAILYSENSE/js/index.js
@@ -5,9 +5,9 @@ tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
 tl.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
 //tl.to('.slider1', { y: '-100%', duration: 1.5, delay: 0.5 });
 tl.to('.intro', { y: '-100%', duration: 1 }, "-=1");
-tl.fromTo('.blob-top', { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo('.blob2', { opacity: 0 }, { opacity: 1, duration: 1 }, );
 tl.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+tl.fromTo('.blob2', { opacity: 0 }, { opacity: 1, duration: 1 }, );
+tl.fromTo('.blob-top', { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo('.content', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
 
 //custom cursor
